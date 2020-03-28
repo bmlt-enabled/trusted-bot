4dd8069eadd49672faada81de928188db049622b
Danny <danny.gershman@gmail.com>
2020-03-28 00:35:28 -0400

    testing timed jft

--- scripts/jft.js
+++ scripts/jft.js
@@ -5,7 +5,7 @@
 //   None
 //
 // Configuration:
-//   None
+//   JFT_ROOM
 //
 // Commands:
 //   hubot jft
@@ -13,13 +13,29 @@
 // Author:
 //   radius314
 
+var CronJob = require('cron').CronJob;
+
 module.exports = function(robot) {
+  if (process.env.JFT_ROOM) {
+    var job = new CronJob('0 0/1 * * * *', function () {
+      sendJft(function (err, res, body) {
+        robot.messageRoom(process.env.JFT_ROOM, "```" + strip_html_tags(body) + "```");
+      });
+    }, null, true, 'America/New_York');
+    job.start();
+  }
 
   robot.respond(/jft/i, (msg) => {
-    robot.http("https://www.jftna.org/jft/").get()((err, res, body) => {
+    sendJft(function(err, res, body) {
       msg.send("```" + strip_html_tags(body) + "```");
-    })
+    });
   });
+
+  function sendJft(callback) {
+    robot.http("https://www.jftna.org/jft/").get()((err, res, body) => {
+      callback(err, res, body);
+    })
+  }
 
   function strip_html_tags(str) {
     if ((str===null) || (str==='')) {
