9bf6daf7e77ca573f5263bd017000eadee588ea6
Danny Gershman <danny.gershman@gmail.com>
2020-04-15 19:17:38 -0400

    adding discord only meetings

--- scripts/meetings.js
+++ scripts/meetings.js
@@ -19,35 +19,41 @@ module.exports = function(robot) {
   var api_token = process.env.GOOGLE_API_TOKEN;
   let virtualYapServer = process.env.VIRTUAL_YAP_SERVER;
 
+  function sendResults(query) {
+    robot.http(query).get()((err, res, body) => {
+      parseString(body, function (err, result) {
+        let data = result["Response"]["Say"];
+        let chunk = 0;
+        let messageText = "";
+        for (let i = 1; i < data.length - 1; i++) {
+          if (chunk === 0) {
+            chunk++;
+            continue;
+          }
+
+          chunk++;
+          messageText += data[i]["_"] + " ";
+          if (chunk === 4) {
+            msg.send(messageText);
+            chunk = 0;
+            messageText = "";
+          }
+        }
+      });
+    })
+  }
+
+  robot.respond(/thisvm/i, (msg) => {
+    sendResults(`${virtualYapServer}/meeting-search.php?Latitude=35.5648713&Longitude=-78.6682395&result_count_max=10&suppress_voice_results=false&custom_query=%26services%3D15amp;{DAY}`)
+  });
+
   robot.respond(/vm (.*)/i, (msg) => {
     var address = encodeURIComponent(msg.match[1]);
     robot.http(`https://maps.googleapis.com/maps/api/geocode/json?key=${api_token}&address=${address}&components=country:us`).get()((err, res, body) => {
       let result = JSON.parse(body)['results'][0];
       let latitude = result['geometry']['location']['lat'];
       let longitude = result['geometry']['location']['lng'];
-      let query = `${virtualYapServer}/meeting-search.php?Latitude=${latitude}&Longitude=${longitude}&result_count_max=10&suppress_voice_results=false`;
-      console.log(query);
-      robot.http(query).get()((err, res, body) => {
-        parseString(body, function (err, result) {
-          let data = result["Response"]["Say"];
-          let chunk = 0;
-          let messageText = "";
-          for (let i = 1; i < data.length - 1; i++) {
-            if (chunk === 0) {
-              chunk++;
-              continue;
-            }
-
-            chunk++;
-            messageText += data[i]["_"] + " ";
-            if (chunk === 4) {
-              msg.send(messageText);
-              chunk = 0;
-              messageText = "";
-            }
-          }
-        });
-      })
+      sendResults(`${virtualYapServer}/meeting-search.php?Latitude=${latitude}&Longitude=${longitude}&result_count_max=10&suppress_voice_results=false`);
     });
   });
 };
