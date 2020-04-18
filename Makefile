12678e0abd9d12cb616305fa41f5b20d1184a094
Danny Gershman <danny.gershman@gmail.com>
2020-04-18 00:26:17 -0400

    setting auto playback of upcoming meetings

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
-.PHONY: run run-as-discord build
+.PHONY: run run-as-discord build clean
 
 build:
 	docker-compose build
+
+clean:
+	docker rmi trusted-bot_trustedbot
 
 run: # https://github.com/docker/compose/issues/1259
 	docker-compose run -e INSPECT=true -e ADAPTER=shell -e HUBOT_NAME=trusted-bot --service-ports trustedbot
 
 run-as-discord:
 	# run as test bot "trusted-bot", turn debugger on by default
-	docker-compose run -e INSPECT=true -e ADAPTER=discord -e HUBOT_NAME=trusted-bot --service-ports trustedbot
+	docker-compose run -e INSPECT=true -e ADAPTER=discord -e HUBOT_NAME=trusted-bot-labs --service-ports trustedbot
--- scripts/meetings.js
+++ scripts/meetings.js
@@ -7,17 +7,38 @@
 // Configuration:
 //   GOOGLE_API_TOKEN
 //   VIRTUAL_YAP_SERVER
+//   VIRTUAL_YAP_SERVER_SERVICE_BODY_ID
+//   ANNOUNCEMENT_GRACE_MINS
+//   MEETING_SCHEDULE_ROOM
+//   DEFAULT_SERVER_TIMEZONE
+//   DEFAULT_SERVER_LATITUDE
+//   DEFAULT_SERVER_LONGITUDE
 //
 // Commands:
 //   hubot vm <location> - Returns a selection of upcoming virtual meetings with the timezone adjustment based of entered location.
+//   hubot vmthis - Returns the meetings for the service body ID for this particular server from VIRTUAL_YAP_SERVER.
 //
 // Author:
 //   radius314
+
 var parseString = require('xml2js').parseString;
+var moment = require('moment');
+var CronJob = require('cron').CronJob;
 
 module.exports = function(robot) {
   var api_token = process.env.GOOGLE_API_TOKEN;
   let virtualYapServer = process.env.VIRTUAL_YAP_SERVER;
+  let announcementGraceMins = process.env.ANNOUNCEMENT_GRACE_MINS;
+
+  if (process.env.MEETING_SCHEDULE_ROOM) {
+    console.log(`Starting Meeting Schedule timer for ${process.env.MEETING_SCHEDULE_ROOM}.`);
+    var job = new CronJob('30 * * * * *', function () {
+      sendJft(function (err, res, body) {
+        robot.messageRoom(process.env.MEETING_SCHEDULE_ROOM, "```" + strip_html_tags(body) + "```");
+      });
+    }, null, true, process.env.DEFAULT_SERVER_TIMEZONE);
+    job.start();
+  }
 
   function sendResults(query, callback) {
     robot.http(query).get()((err, res, body) => {
@@ -44,9 +65,24 @@ module.exports = function(robot) {
   }
 
   robot.respond(/thisvm/i, (msg) => {
-    sendResults(`${virtualYapServer}/meeting-search.php?Latitude=35.5648713&Longitude=-78.6682395&result_count_max=5&suppress_voice_results=false&custom_query=%26services%3D15amp;{DAY}`, results => {
-      msg.send(results)
-    })
+    moment.tz.setDefault(process.env.DEFAULT_SERVER_TIMEZONE);
+    let today = moment().isoWeekday();
+    let serviceBodyId = process.env['VIRTUAL_YAP_SERVER_SERVICE_BODY_ID'];
+    let query = `${process.env.VIRTUAL_YAP_SERVER}/api/getMeetings.php?results_count=5&suppress_voice_results=false&custom_query=%26services%3D${serviceBodyId}amp;{DAY}&latitude=${process.env.DEFAULT_SERVER_LATITUDE}&longitude=${process.env.DEFAULT_SERVER_LONGITUDE}`;
+    robot.http(query).get()((err, res, body) => {
+      for (let meeting of JSON.parse(body)['filteredList']) {
+        let time = moment(meeting['start_time'], "HH:mm A");
+        let day_id = meeting['weekday_tinyint'] - 1;
+        let date_stamp = today <= day_id ? moment().isoWeekday(day_id) : moment().add(1, 'weeks').isoWeekday(day_id);
+        date_stamp.set({
+          hour: time.get('hour'),
+          minute: time.get('minute')
+        });
+        if (date_stamp.diff(moment(), 'minutes') === announcementGraceMins) {
+          robot.messageRoom(`TESTING!!! ${meeting['meeting_name']} meeting soon at ${date_stamp.format("hh:mm A z")}.  Click the HTTPS link to be brought into the voice channel.\n\nDon't forget to mute your mic.\n\n${meeting['comments']}`)
+        }
+      }
+    });
   });
 
   robot.respond(/vm (.*)/i, (msg) => {
