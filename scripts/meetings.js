afd16c524e41c241dcee66731bb37f715a17da43
Danny Gershman <danny.gershman@gmail.com>
2020-04-18 00:59:42 -0400

    handling next meeting(s), fixing cron job

--- scripts/meetings.js
+++ scripts/meetings.js
@@ -33,11 +33,35 @@ module.exports = function(robot) {
   if (process.env.MEETING_SCHEDULE_ROOM) {
     console.log(`Starting Meeting Schedule timer for ${process.env.MEETING_SCHEDULE_ROOM}.`);
     var job = new CronJob('30 * * * * *', function () {
-      sendJft(function (err, res, body) {
-        robot.messageRoom(process.env.MEETING_SCHEDULE_ROOM, "```" + strip_html_tags(body) + "```");
+      thisVirtualMeetings((err, res, body) => {
+        for (let meeting of JSON.parse(body)['filteredList']) {
+          moment.tz.setDefault(process.env.DEFAULT_SERVER_TIMEZONE);
+          let date_stamp = nextInstanceOfMeeting(meeting);
+          if (date_stamp.diff(moment(), 'minutes') === announcementGraceMins) {
+            robot.messageRoom(`TESTING!!! ${meeting['meeting_name']} meeting soon at ${date_stamp.format("hh:mm A z")}.  Click the HTTPS link to be brought into the voice channel.\n\nDon't forget to mute your mic.\n\n${meeting['comments']}`)
+          }
+        }
       });
     }, null, true, process.env.DEFAULT_SERVER_TIMEZONE);
     job.start();
+  }
+
+  function nextInstanceOfMeeting(meeting) {
+    let today = moment().isoWeekday();
+    let time = moment(meeting['start_time'], "HH:mm A");
+    let day_id = meeting['weekday_tinyint'] - 1;
+    let date_stamp = today <= day_id ? moment().isoWeekday(day_id) : moment().add(1, 'weeks').isoWeekday(day_id);
+    date_stamp.set({
+      hour: time.get('hour'),
+      minute: time.get('minute')
+    });
+    return date_stamp;
+  }
+
+  function thisVirtualMeetings(callback) {
+    let serviceBodyId = process.env['VIRTUAL_YAP_SERVER_SERVICE_BODY_ID'];
+    let query = `${process.env.VIRTUAL_YAP_SERVER}/api/getMeetings.php?results_count=5&suppress_voice_results=false&custom_query=%26services%3D${serviceBodyId}amp;{DAY}&latitude=${process.env.DEFAULT_SERVER_LATITUDE}&longitude=${process.env.DEFAULT_SERVER_LONGITUDE}`;
+    robot.http(query).get()(callback);
   }
 
   function sendResults(query, callback) {
@@ -65,24 +89,17 @@ module.exports = function(robot) {
   }
 
   robot.respond(/thisvm/i, (msg) => {
-    moment.tz.setDefault(process.env.DEFAULT_SERVER_TIMEZONE);
-    let today = moment().isoWeekday();
-    let serviceBodyId = process.env['VIRTUAL_YAP_SERVER_SERVICE_BODY_ID'];
-    let query = `${process.env.VIRTUAL_YAP_SERVER}/api/getMeetings.php?results_count=5&suppress_voice_results=false&custom_query=%26services%3D${serviceBodyId}amp;{DAY}&latitude=${process.env.DEFAULT_SERVER_LATITUDE}&longitude=${process.env.DEFAULT_SERVER_LONGITUDE}`;
-    robot.http(query).get()((err, res, body) => {
-      for (let meeting of JSON.parse(body)['filteredList']) {
-        let time = moment(meeting['start_time'], "HH:mm A");
-        let day_id = meeting['weekday_tinyint'] - 1;
-        let date_stamp = today <= day_id ? moment().isoWeekday(day_id) : moment().add(1, 'weeks').isoWeekday(day_id);
-        date_stamp.set({
-          hour: time.get('hour'),
-          minute: time.get('minute')
-        });
-        if (date_stamp.diff(moment(), 'minutes') === announcementGraceMins) {
-          robot.messageRoom(`TESTING!!! ${meeting['meeting_name']} meeting soon at ${date_stamp.format("hh:mm A z")}.  Click the HTTPS link to be brought into the voice channel.\n\nDon't forget to mute your mic.\n\n${meeting['comments']}`)
+    thisVirtualMeetings((err, res, body) => {
+      var meetings = JSON.parse(body)['filteredList'];
+      for (let i = 0; i < meetings.length; i++) {
+        let meeting = meetings[i];
+        let date_stamp = nextInstanceOfMeeting(meeting);
+        if (meetings[i]['start_time'] === meetings[0]['start_time']
+            && meetings[i]['weekday_tinyint'] === meetings[0]['weekday_tinyint']) {
+          msg.send(`${meeting['meeting_name']} meeting is next on ${date_stamp.format("dddd, MMMM Do, YYYY hh:mm A z")}.  Click the HTTPS link to be brought into the voice channel.\n\nDon't forget to mute your mic.\n\n${meeting['comments']}`)
         }
       }
-    });
+    })
   });
 
   robot.respond(/vm (.*)/i, (msg) => {
