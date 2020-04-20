dd92ef871c80e8871194d325cb64996f72e5ccb2
Danny Gershman <danny.gershman@gmail.com>
2020-04-20 00:04:37 -0400

    remove console messages

--- scripts/meetings.js
+++ scripts/meetings.js
@@ -38,7 +38,6 @@ module.exports = robot => {
         for (let meeting of JSON.parse(body)['filteredList']) {
           let date_stamp = nextInstanceOfMeeting(meeting);
           let diffMins = date_stamp.diff(moment(), 'minutes');
-          console.log(`meetings.js diffMins: ${diffMins}`);
           if (diffMins < parseInt(announcementGraceMins) && !hasReminderBeenSent(meeting['id_bigint'])) {
             markReminderSent(meeting['id_bigint']);
             robot.messageRoom(process.env.MEETING_SCHEDULE_ROOM, `TESTING!!! ${meeting['meeting_name']} meeting soon at ${date_stamp.format("hh:mm A z")}.  Click the HTTPS link to be brought into the voice channel.\n\nDon't forget to mute your mic.\n\n${meeting['comments']}`)
